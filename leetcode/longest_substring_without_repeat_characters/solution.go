57a4a77b0b903b3f4b4e87f6817b70da5891d151
spin <spin637@gmail.com>
2022-12-25 10:21:15 +0800

    sliding window version

--- leetcode/longest_substring_without_repeat_characters/solution.go
+++ leetcode/longest_substring_without_repeat_characters/solution.go
@@ -2,43 +2,70 @@ package main
 
 // import "fmt"
 
-func main() {}
+// func lengthOfLongestSubstring(s string) int {
+// 	sArr := []rune(s)
+// 	checkExist := make(map[rune]bool)
+// 	indexMap := make(map[rune]int)
+// 	pos := 0
+// 	temp := 0
+// 	max := 0
+// 	for pos < len(sArr) {
+// 		c := sArr[pos]
+// 		if !checkExist[c] {
+// 			checkExist[c] = true
+// 			indexMap[c] = pos
+// 			temp++
+// 			// fmt.Printf("%c %d\n", c, temp)
+// 		} else {
+// 			// fmt.Printf("repeat! %c %d %d\n", c, pos, indexMap[c])
+// 			if temp > max {
+// 				max = temp
+// 			}
+// 			// checkExist = make(map[rune]bool)
+// 			clear(checkExist)
+// 			pos = indexMap[c]
+// 			temp = 0
+// 		}
+// 		pos += 1
+// 	}
+// 	if temp > max {
+// 		max = temp
+// 	}
+
+// 	return max
+// }
+
+// func clear(m map[rune]bool) {
+// 	for k := range m {
+// 		m[k] = false
+// 	}
+// }
 
 func lengthOfLongestSubstring(s string) int {
-	sArr := []rune(s)
-	checkExist := make(map[rune]bool)
-	indexMap := make(map[rune]int)
-	pos := 0
-	temp := 0
-	max := 0
-	for pos < len(sArr) {
-		c := sArr[pos]
-		if !checkExist[c] {
-			checkExist[c] = true
-			indexMap[c] = pos
-			temp++
-			// fmt.Printf("%c %d\n", c, temp)
-		} else {
-			// fmt.Printf("repeat! %c %d %d\n", c, pos, indexMap[c])
-			if temp > max {
-				max = temp
-			}
-			// checkExist = make(map[rune]bool)
-			clear(checkExist)
-			pos = indexMap[c]
-			temp = 0
+	sa := []rune(s)
+	chars := make(map[rune]int)
+	left := 0
+	right := 0
+	res := 0
+	for right < len(sa) {
+		r := sa[right]
+		chars[r] += 1
+		for chars[r] > 1 {
+			l := sa[left]
+			chars[l] -= 1
+			left += 1
 		}
-		pos += 1
-	}
-	if temp > max {
-		max = temp
-	}
 
-	return max
+		res = max(res, (right - left + 1))
+		right += 1
+	}
+	return res
 }
 
-func clear(m map[rune]bool) {
-	for k := range m {
-		m[k] = false
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
 	}
 }
