ea9cb7c0b246728e3ea8fd4f6e1072bba7438b12
spin <spin637@gmail.com>
2022-12-25 10:49:22 +0800

    preassume ASCII code

--- leetcode/longest_substring_without_repeat_characters/solution.go
+++ leetcode/longest_substring_without_repeat_characters/solution.go
@@ -41,26 +41,42 @@ package main
 // 	}
 // }
 
-func lengthOfLongestSubstring(s string) int {
-	sa := []rune(s)
-	chars := make(map[rune]int)
-	left := 0
-	right := 0
-	res := 0
-	for right < len(sa) {
-		r := sa[right]
-		chars[r] += 1
-		for chars[r] > 1 {
-			l := sa[left]
-			chars[l] -= 1
-			left += 1
-		}
+// func lengthOfLongestSubstring(s string) int {
+// 	sa := []rune(s)
+// 	chars := make(map[rune]int)
+// 	left := 0
+// 	right := 0
+// 	res := 0
+// 	for right < len(sa) {
+// 		r := sa[right]
+// 		chars[r] += 1
+// 		for chars[r] > 1 {
+// 			l := sa[left]
+// 			chars[l] -= 1
+// 			left += 1
+// 		}
 
-		res = max(res, (right - left + 1))
-		right += 1
-	}
-	return res
-}
+// 		res = max(res, (right - left + 1))
+// 		right += 1
+// 	}
+// 	return res
+// }
+// func lengthOfLongestSubstring(s string) int {
+// 	sa := []rune(s)
+// 	n := len(s)
+// 	ans := 0
+// 	mp := make(map[rune]int)
+// 	i := 0
+// 	for j := 0; j < n; j++ {
+// 		if v, ok := mp[sa[j]]; ok {
+// 			i = max(v, i)
+// 		}
+
+// 		ans = max(ans, j-i)
+// 		mp[sa[j]] = j
+// 	}
+// 	return ans
+// }
 
 func max(a int, b int) int {
 	if a > b {
@@ -68,4 +84,26 @@ func max(a int, b int) int {
 	} else {
 		return b
 	}
+}
+
+func lengthOfLongestSubstring(s string) int {
+	sa := []rune(s)
+	chars := [128]int{}
+	for i := 0; i < len(chars); i++ {
+		chars[i] = -1
+	}
+	left := 0
+	right := 0
+	res := 0
+	for right < len(s) {
+		r := sa[right]
+		index := chars[int(r)]
+		if index != -1 && left <= index && index < right {
+			left = index + 1
+		}
+		res = max(res, right-left+1)
+		chars[int(r)] = right
+		right += 1
+	}
+	return res
 }
