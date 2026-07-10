1983dc7fa176b368ad42548572661b69818dd160
Spin Liao <spin.liao@btse.com>
2026-07-11 00:01:15 +0800

    13. Roman to Integer

---
+++ new_leetcode/13_roman_to_integer/go.mod
@@ -0,0 +1,3 @@
+module example/13_roman_to_integer
+
+go 1.26.4
---
+++ new_leetcode/13_roman_to_integer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func romanToInt(s string) int {
+	m := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	res := 0
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && m[s[i]] < m[s[i+1]] {
+			res -= m[s[i]]
+		} else {
+			res += m[s[i]]
+		}
+	}
+
+	return res
+}
+
+func main() {
+	res := romanToInt("MCMXCIV")
+	fmt.Println(res)
+
+	res = romanToInt("LVIII")
+	fmt.Println(res)
+
+	res = romanToInt("III")
+	fmt.Println(res)
+}
